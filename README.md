e37a711f8fac76fc1d4bc5dae73bb9f29b3992cf
Taha <55713590+TahaMetin@users.noreply.github.com>
2021-03-30 19:56:18 +0300

    Color changing System

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Slightly Color Changing System
- Change color slightly on every trigger.
+ Change material color and light color (light color more translucent)slightly on every trigger.
+ I use this system at one of my games [click for see](https://play.google.com/store/apps/details?id=com.YeasWorld.FitIt)
+ 
+ 
+ 
+##Material color RGB numbers cycle:
+R 	G 	B
+
+250	0	0
+250	0	250
+0	0	250
+0	250	250	
+0	250	0
+250	250	0
+250 0	0
+
+In my case i set it to start over in 100 steps. so :
+
+250 * 6 = 1500
+1500 / 100 = 15
+ 
+ 
+##Light color RGB numbers cycle:
+ 
+R	G	B
+
+250	200	200
+250	200	250
+200	200	250
+200	250	250
+200	250	200
+250	250	200
+250	200	200
+
+50 * 6 = 300
+300 / 100 = 3
+
---
+++ RenkDegistir.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenkDegistir : MonoBehaviour  
+{
+    public new GameObject light;
+    public Material playerColor;
+    int rPlayer, gPlayer, bPlayer;
+    int rLight, gLight, bLight;
+    private void Start()
+    {
+        int random =Random.Range(0, 6);		// Assigns one of 6 different colors as the first color
+        Debug.Log(random);					// Based on rgb color system
+        switch (random)
+        {
+            case 0:
+                rPlayer = 255;
+                gPlayer = 0;
+                bPlayer = 0;
+                rLight = 250;
+                gLight = 200;
+                bLight = 200;
+                break;
+            case 1:
+                rPlayer = 255;
+                gPlayer = 0;
+                bPlayer = 255;
+                rLight = 250;
+                gLight = 200;
+                bLight = 250;
+                break;
+            case 2:
+                rPlayer = 0;
+                gPlayer = 0;
+                bPlayer = 255;
+                rLight = 200;
+                gLight = 200;
+                bLight = 250;
+                break;
+            case 3:
+                rPlayer = 0;
+                gPlayer = 255;
+                bPlayer = 255;
+                rLight = 200;
+                gLight = 250;
+                bLight = 250;
+                break;
+            case 4:
+                rPlayer = 0;
+                gPlayer = 255;
+                bPlayer = 0;
+                rLight = 200;
+                gLight = 250;
+                bLight = 200;
+                break;
+            case 5:
+                rPlayer = 255;
+                gPlayer = 255;
+                bPlayer = 0;
+                rLight = 250;
+                gLight = 250;
+                bLight = 200;
+                break;
+        }
+        BlokRenkDegistirici(new Color(colorNumberConversion(rPlayer), colorNumberConversion(gPlayer), colorNumberConversion(bPlayer)));
+        light.GetComponent<Light>().color = new Color(colorNumberConversion(rLight), colorNumberConversion(gLight), colorNumberConversion(bLight));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("GecmeDenetleyici"))
+        {
+            RenkDegistirici();
+        }
+    }
+    private float colorNumberConversion(int num) // unity takes rgb numbers as floats between 0-1
+    {
+        return num / 255.0f;
+    }
+
+    private void BlokRenkDegistirici(Color color)		// changes the color of the materials
+    {
+        Renderer[] renderers= gameObject.transform.parent.GetComponentsInChildren<Renderer>();
+        foreach(Renderer r in renderers)
+        {
+            r.material.color = color;
+        }
+    }
+
+    void RenkDegistirici()  // determines the next color 
+    {
+        if (rPlayer >= 255 && gPlayer == 0 && bPlayer < 255 && bPlayer >= 0)
+        {
+            bPlayer += 15;
+            bLight += 3;
+        } else if (rPlayer > 0 && rPlayer <= 255 && gPlayer == 0 && bPlayer >= 255)
+        {
+            rPlayer -= 15;
+            rLight -= 3;
+        } else if (rPlayer == 0 && 0 <= gPlayer && gPlayer < 255 && bPlayer >= 255)
+        {
+            gPlayer += 15;
+            gLight += 3;
+        }else if (rPlayer == 0 && gPlayer >= 255 && 0<bPlayer && bPlayer <= 255)
+        {
+            bPlayer -= 15;
+            bLight -= 3;
+        }else if (0<= rPlayer && rPlayer < 255 && gPlayer >= 255 && bPlayer == 0)
+        {
+            rPlayer += 15;
+            rLight += 3;
+        }else if(rPlayer >= 255 && 0< gPlayer && gPlayer<= 255 && bPlayer == 0)
+        {
+            gPlayer -= 15;
+            gLight -= 3;
+        }
+
+        BlokRenkDegistirici(new Color(colorNumberConversion(rPlayer), colorNumberConversion(gPlayer), colorNumberConversion(bPlayer)));
+        light.GetComponent<Light>().color = new Color(colorNumberConversion(rLight), colorNumberConversion(gLight), colorNumberConversion(bLight));
+    }
+
+
+}
